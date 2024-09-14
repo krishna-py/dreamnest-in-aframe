b3f433443f3924a1a880184b84814c967c5cac67
Krishna Prasad Yellapragada <krishna.yellapragada@students.iiit.ac.in>
2024-09-14 23:17:06 +0530

    Add levitate component and update property home scene

---
+++ levitate.js
@@ -0,0 +1,9 @@
+
+AFRAME.registerComponent('levitate', {
+    tick: function (t, dt) {
+      var mesh = this.el.getObject3D('mesh');
+      if (!mesh) return;
+      mesh.rotation.y += 0.1 * dt / 1000;
+      mesh.position.y = 0.25 * Math.sin(t / 1000);
+    }
+  });
